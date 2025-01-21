4ef55fb82b5a3d3e99843e554bf7aa1ec38aec4b
abisara2k1 <abisara@iu.edu>
2025-01-21 02:15:16 -0500

    practicum 1 challenge completed

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,31 +1,30 @@
 package com.example.lab1
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Toast
-
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        trueButton.setOnClickListener {
-                view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
+
+        trueButton.setOnClickListener { view: View ->
+            Snackbar.make(
+                view, // Use the clicked button as the anchor view
+                R.string.correct_toast, // Replace with the same string resource
+                Snackbar.LENGTH_SHORT
             ).show()
         }
     }
