38f46469df1254b18fe6a3cce53007b48aaadbab
Abirami Saravanan <abiramisaravanan@Abiramis-MacBook-Pro.local>
2025-01-21 02:08:42 -0500

    lab1 completed

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.lab1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
