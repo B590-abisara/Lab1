b2b014867b66d6665dc2b0fc9d2c119019286fdd
abisara2k1 <abisara@iu.edu>
2025-02-17 16:02:18 -0500

    homework2-part2-challenge2 completed

--- app/src/main/java/com/example/lab1/CheatActivity.kt
+++ app/src/main/java/com/example/lab1/CheatActivity.kt
@@ -9,11 +9,12 @@ import com.example.lab1.databinding.ActivityCheatBinding
 
 private const val EXTRA_ANSWER_IS_TRUE = "answer_is_true"
 private const val EXTRA_ANSWER_SHOWN = "answer_shown"
+private const val KEY_ANSWER_SHOWN = "key_answer_shown"
 
 class CheatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCheatBinding
     private var answerIsTrue = false
-
+    private var isAnswerShown = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,22 +22,38 @@ class CheatActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        isAnswerShown = savedInstanceState?.getBoolean(KEY_ANSWER_SHOWN, false) ?: false
 
-        binding.showAnswerButton.setOnClickListener {
-            val answerText = when {
-                answerIsTrue -> R.string.true_button
-                else -> R.string.false_button
-            }
-            binding.answerTextView.setText(answerText)
+        if (isAnswerShown) {
+            showAnswer()
             setAnswerShownResult(true)
         }
 
+        binding.showAnswerButton.setOnClickListener {
+            showAnswer()
+            setAnswerShownResult(true)
+        }
     }
+
+    private fun showAnswer() {
+        val answerText = when {
+            answerIsTrue -> R.string.true_button
+            else -> R.string.false_button
+        }
+        binding.answerTextView.setText(answerText)
+        isAnswerShown = true
+    }
+
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
         val data = Intent().apply {
             putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
         }
         setResult(Activity.RESULT_OK, data)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY_ANSWER_SHOWN, isAnswerShown)
     }
 
     companion object {
@@ -47,4 +64,3 @@ class CheatActivity : AppCompatActivity() {
         }
     }
 }
-
